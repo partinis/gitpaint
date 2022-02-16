467adc065fee1c2cc4db2646b96d950f48e5512d
partinis <partinis@gmx.de>
2022-02-16 16:19:52 +0100

    new

--- dist/index.js
+++ dist/index.js
@@ -32,24 +32,6 @@ function run() {
         // Now read in the files with the function defined above
         const userName = core.getInput("github_user_name");
         console.log(githubToken + "-" + userName);
-        // the context does for example also include information
-        // in the pull request or repository we are issued from
-        const context = github.context;
-        const repo = context.repo;
-        const pullRequestNumber = context.payload.pull_request.number;
-        // The Octokit is a helper, to interact with
-        // the github REST interface.
-        // You can look up the REST interface
-        // here: https://octokit.github.io/rest.js/v18
-        const octokit = github.getOctokit(githubToken);
-        // Get all comments we currently have...
-        // (this is an asynchronous function)
-        const { data: comments } = yield octokit.issues.listComments(Object.assign(Object.assign({}, repo), { issue_number: pullRequestNumber }));
-        // ... and check if there is already a comment by us
-        const comment = comments.find((comment) => {
-            return (comment.user.login === "github-actions[bot]" &&
-                comment.body.startsWith("## Result of Benchmark Tests\n"));
-        });
     });
 }
 // Our main method: call the run() function and report any errors
