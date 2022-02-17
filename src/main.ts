48653d7ae24fcc2a9388c4d58eebad15a72b4b70
partinis <partinis@gmx.de>
2022-02-17 18:32:36 +0100

    new

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,15 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+// const github = require("@actions/github");
+import { loadSite } from "./loadSite";
+import { generateSvg } from "./generateSvg";
 
 async function run() {
-    // if (github.context.eventName !== "pull_request") {
-    //     // The core module on the other hand let's you get
-    //     // inputs or create outputs or control the action flow
-    //     // e.g. by producing a fatal error
-    //     core.setFailed("Can only run on pull requests!");
-    //     return;
-    // }
-
-    // get the inputs of the action. The "token" input
-    // is not defined so far - we will come to it later.
-    const githubToken = core.getInput("token");
-
-    // Now read in the files with the function defined above
-    const userName = core.getInput("github_user_name");
-    console.log(githubToken+"-"+userName);
-
+  generateSvg(
+    await loadSite(core.getInput("github_user_name")),
+    core.getInput("text"),
+    core.getInput("svg_file_name")
+  );
 }
 
-// Our main method: call the run() function and report any errors
-run().catch(error => core.setFailed("Workflow failed! " + error.message));
+// Call the run() function and report any errors
+run().catch((error) => core.setFailed("Workflow failed! " + error.message));
